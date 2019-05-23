0a4cbe850aeb717b796d3cd1d7b603a46039b2d3
浮生若梦 <microease@163.com>
2019-05-23 11:15:26 +0800

    add

--- 1_1_Java基础语法/DataType.java
+++ 1_1_Java基础语法/DataType.java
@@ -3,5 +3,9 @@ public class DataType {
         System.out.println(1024);
         long num1 = 100;
         System.out.println(num1);
+        double num2 = 2.5F;
+        System.out.println(num2);
+        int num3 = 100L;
+        System.out.println(num3);
     }
 }
