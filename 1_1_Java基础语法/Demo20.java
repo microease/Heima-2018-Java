c9222657181db7c9eb5fe57754f34fd55b9c6f60
浮生若梦 <microease@163.com>
2019-05-27 15:55:57 +0800

    add

--- 1_1_Java基础语法/Demo20.java
+++ 1_1_Java基础语法/Demo20.java
@@ -1,7 +1,14 @@
 public class Demo20 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 20; i++) {
-            System.out.println(i);
+        printStar();
+    }
+
+    public static void printStar() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 0; j <= 20; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
---
+++ 1_1_Java基础语法/Demo21.java
@@ -0,0 +1,11 @@
+public class Demo21 {
+    public static void main(String[] args) {
+        sum(1, 2);
+    }
+
+    public static int sum(int a, int b) {
+        int result = a + b;
+        System.out.println(result);
+        return result;
+    }
+}
