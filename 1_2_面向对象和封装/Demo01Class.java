fd53601588aa39de5415c7cd1e183b0ae91987a3
浮生若梦 <microease@163.com>
2019-05-29 18:03:43 +0800

    add

---
+++ 1_2_面向对象和封装/Demo01Class.java
@@ -0,0 +1,7 @@
+import Student;
+public class Demo01Class {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ 1_2_面向对象和封装/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    String name;
+    int age;
+
+    public void eat() {
+        System.out.println("吃饭");
+    }
+
+    public void sleep() {
+        System.out.println("睡觉");
+    }
+}
