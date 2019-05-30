073aff07032bf1c2b46dbe7e810e503aab0be549
浮生若梦 <microease@163.com>
2019-05-30 18:04:59 +0800

    add

--- 1_2_面向对象和封装/demo05/Demo01.java
+++ 1_2_面向对象和封装/demo05/Demo01.java
@@ -4,5 +4,6 @@ public class Demo01 {
     public static void main(String[] args) {
         Student stu1 = new Student();
         Student stu2 = new Student("赵丽颖",20);
+        System.out.println(stu2.getName()+stu2.getAge());
     }
 }
