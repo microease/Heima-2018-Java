f2d95d9745c9f3c284a3905a67de39a7fa49a0ea
浮生若梦 <microease@163.com>
2019-06-08 15:50:25 +0800

    add

--- 1_4_继承与多态/src/com.wzxdm/demo10/demo01InnerClass.java
+++ 1_4_继承与多态/src/com.wzxdm/demo10/demo01InnerClass.java
@@ -1,5 +1,8 @@
 package src.com.wzxdm.demo10;
 
 public class demo01InnerClass {
-
+    public static void main(String[] args) {
+        Body body = new Body();
+        body.methodBody();
+    }
 }
