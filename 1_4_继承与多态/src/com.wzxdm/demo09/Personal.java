05bba298d08f6d963e2738c9d4c3f66726a0fdcc
浮生若梦 <microease@163.com>
2019-06-08 11:09:08 +0800

    add

---
+++ 1_4_继承与多态/src/com.wzxdm/demo09/Personal.java
@@ -0,0 +1,21 @@
+package src.com.wzxdm.demo09;
+
+public class Personal {
+    public final String name;
+
+    public Personal() {
+        name = "关晓彤";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+
+    public Personal(String name) {
+        this.name = name;
+    }
+}
