cbb62cf2370da8ff172db8530577110de257f428
浮生若梦 <microease@163.com>
2019-06-08 16:15:27 +0800

    add

---
+++ 1_4_继承与多态/src/com.wzxdm/demo10/Outer.java
@@ -0,0 +1,14 @@
+package src.com.wzxdm.demo10;
+
+public class Outer {
+    int num = 10;
+    public class Inner{
+        int num =20;
+        public void methodInner(){
+            int num = 30;
+            System.out.println(num);
+            System.out.println(this.num);
+            System.out.println();
+        }
+    }
+}
--- 1_4_继承与多态/src/com.wzxdm/demo10/demo01InnerClass.java
+++ 1_4_继承与多态/src/com.wzxdm/demo10/demo01InnerClass.java
@@ -4,5 +4,8 @@ public class demo01InnerClass {
     public static void main(String[] args) {
         Body body = new Body();
         body.methodBody();
+        System.out.println("+++++++++++++++++++");
+        Body.Heart heart = new Body().new Heart();
+        heart.beat();
     }
 }
