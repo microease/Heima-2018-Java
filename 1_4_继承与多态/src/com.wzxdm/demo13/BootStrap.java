f8a65d985dd9cd700effc49712b0bc9be4160e66
浮生若梦 <microease@163.com>
2019-06-09 14:47:13 +0800

    add

---
+++ 1_4_继承与多态/src/com.wzxdm/demo13/BootStrap.java
@@ -0,0 +1,4 @@
+package src.com.wzxdm.demo13;
+
+public class BootStrap {
+}
---
+++ 1_4_继承与多态/src/com.wzxdm/red/OpenMode.java
@@ -0,0 +1,7 @@
+package src.com.wzxdm.red;
+
+import java.util.ArrayList;
+
+public interface OpenMode {
+    ArrayList<Integer> divide(int totalMoney,int totalCount);
+}
