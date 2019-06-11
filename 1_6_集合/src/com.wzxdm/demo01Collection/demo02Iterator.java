10e81aa395aa7af1ea71f5502688a021249af1d8
浮生若梦 <microease@163.com>
2019-06-11 18:01:26 +0800

    add

--- 1_6_集合/src/com.wzxdm/demo01Collection/demo02Iterator.java
+++ 1_6_集合/src/com.wzxdm/demo01Collection/demo02Iterator.java
@@ -1,5 +1,30 @@
 package src.com.wzxdm.demo01Collection;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
 public class demo02Iterator {
-    
+    public static void main(String[] args) {
+        Collection<String> coll = new ArrayList<>();
+        coll.add("姚明");
+        coll.add("科比");
+        coll.add("麦迪");
+        coll.add("詹姆斯");
+        coll.add("艾弗森");
+        Iterator<String> it = coll.iterator();
+//        boolean b = it.hasNext();
+//        System.out.println(b);
+//        System.out.println(it.next());
+//        System.out.println(it.next());
+//
+//        while (it.hasNext()){
+//            String e = it.next();
+//            System.out.println(e);
+//        }
+
+
+
+
+    }
 }
