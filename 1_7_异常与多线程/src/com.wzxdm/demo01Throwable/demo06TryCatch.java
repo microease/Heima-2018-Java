e2e5eaffd17909d3bd0458025eb02bc9b56fe096
浮生若梦 <microease@163.com>
2019-06-23 18:03:53 +0800

    add

--- 1_7_异常与多线程/src/com.wzxdm/demo01Throwable/demo06TryCatch.java
+++ 1_7_异常与多线程/src/com.wzxdm/demo01Throwable/demo06TryCatch.java
@@ -9,8 +9,13 @@ public class demo06TryCatch  {
             readFile("d:\\a.txt");
         }catch (IOException e){
             System.out.println("传递的文件后缀不是.txt");
+            //Throwable定义了三个异常处理方法
+            System.out.println(e.toString());
+            e.printStackTrace();
         }
+        System.out.println(e.getMessage());
         System.out.println("后续代码");
+        System.out.println(Throwable);
     }
     public static void readFile(String fileName) throws FileNotFoundException {
         if (!fileName.endsWith(".txt")) {
