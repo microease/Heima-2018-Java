1921d7fdab8bb85b2202d630cc6b53c1f9554fdd
浮生若梦 <microease@163.com>
2019-07-04 18:12:00 +0800

    add

--- 1_8_File类与IO流/src/com.wzxdm/Demo01FileClass/Demo02File.java
+++ 1_8_File类与IO流/src/com.wzxdm/Demo01FileClass/Demo02File.java
@@ -1,5 +1,21 @@
 package src.com.wzxdm.Demo01FileClass;
 
+import java.io.File;
+
 public class Demo02File {
-    
+    public static void main(String[] args) {
+        show01();
+
+    }
+    private static void show01(){
+        File f1 = new File("C:\\Users\\microease\\OneDrive - Santa Barbara City College\\2018黑马Java\\1.基础班\\1-8 File类与IO流\\a.txt");
+        System.out.println(f1);
+        File f2 = new File("C:\\Users\\microease\\OneDrive - Santa Barbara City College\\2018黑马Java\\1.基础班\\1-8 File类与IO流\\");
+        System.out.println(f2);
+        File f3 = new File("./Demo01FileClass");
+        System.out.println(f3);
+
+
+
+    }
 }
