93c99b26b961244bb63cee2dab7826cfcf6f90b0
浮生若梦 <microease@163.com>
2019-07-09 15:56:33 +0800

    add

---
+++ 1_8_File类与IO流/src/com.wzxdm/Demo04OutputStream/Demo01OutputStream.java
@@ -0,0 +1,13 @@
+package src.com.wzxdm.Demo04OutputStream;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Demo01OutputStream {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fos = new FileOutputStream("C:\\Users\\microease\\Documents\\GitHub\\Heima-2018-Java\\1_8_File类与IO流\\src\\com.wzxdm\\Demo04OutputStream\\a.txt");
+        fos.write(97);
+        fos.close();
+    }
+}
