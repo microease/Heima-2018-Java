8a6515251b7491afc70ca39546838677aac51569
浮生若梦 <microease@163.com>
2019-07-29 01:06:25 +0800

    add

--- 2_1_基础加强/src/com.wzxdm/Demo02Reflect/Demo02.java
+++ 2_1_基础加强/src/com.wzxdm/Demo02Reflect/Demo02.java
@@ -1,4 +1,11 @@
 package src.com.wzxdm.Demo02Reflect;
 
+import java.lang.reflect.Constructor;
+
 public class Demo02 {
+    public static void main(String[] args) throws Exception {
+        Class personClass = Person.class;
+        Constructor constructor = personClass.getConstructor(String.class, int.class);
+        System.out.println(constructor);
+    }
 }
