fe1e4548117fff9c7cb6a8960085b9f88d9e2e8a
microease <microease@163.com>
2019-08-28 07:43:25 +0800

    啊

---
+++ 2_3_JDBC/Demo01_jdbc/demo01_jdbc.java
@@ -0,0 +1,26 @@
+package Demo01_jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class demo01_jdbc {
+    public static void main(String[] args) throws Exception {
+        //1 导入驱动jar包 add as library
+        //2 注册驱动
+        Class.forName("com.mysql.jdbc.Driver");
+        //3 获取数据库连接对象
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db3", "root", "password");
+        //4 定义sql语句
+        String sql = "update account set balance = 500 where id = 1";
+        //5 获取执行sql的语句 Statement
+        Statement stmt = conn.createStatement();
+        //6 执行sql
+        int count = stmt.executeUpdate(sql);
+        //7 处理结果
+        System.out.println(count);
+        //8 释放资源
+        stmt.close();
+        conn.close();
+    }
+}
