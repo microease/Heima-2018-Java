fdaf6ce45ca3c709b47cc6ba262b37d9f89afdb6
浮生若梦 <microease@163.com>
2019-10-09 15:53:13 +0800

    Update ServerletDemo4.java

--- 2_8_Tomcat/src/cn/wzxdm/web/servlet/ServerletDemo4.java
+++ 2_8_Tomcat/src/cn/wzxdm/web/servlet/ServerletDemo4.java
@@ -3,29 +3,9 @@ package cn.wzxdm.web.servlet;
 import javax.servlet.*;
 import java.io.IOException;
 
-public class ServerletDemo4 implements Servlet {
-    @Override
-    public void init(ServletConfig servletConfig) throws ServletException {
-
-    }
-
-    @Override
-    public ServletConfig getServletConfig() {
-        return null;
-    }
-
+public class ServerletDemo4 extends GenericServlet {
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-
-    }
-
-    @Override
-    public String getServletInfo() {
-        return null;
-    }
-
-    @Override
-    public void destroy() {
-
+        
     }
 }
