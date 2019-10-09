88d6bb346f37cdd04e6383b05ca3a0195fe4cd14
浮生若梦 <microease@163.com>
2019-10-09 16:39:41 +0800

    Update ServerletDemo4.java

--- 2_8_Tomcat/src/cn/wzxdm/web/servlet/ServerletDemo4.java
+++ 2_8_Tomcat/src/cn/wzxdm/web/servlet/ServerletDemo4.java
@@ -1,11 +1,13 @@
 package cn.wzxdm.web.servlet;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
+@WebServlet("/demo2")
 public class ServerletDemo4 extends GenericServlet {
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        
+        System.out.println("demo2..");
     }
 }
